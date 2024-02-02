8adbbcee2df8809b5f8dddfdeeb507e9f16e0184
Alexandre <alexandretavares@outlook.com>
2024-02-01 21:27:47 -0300

    Includes the appsettings.json and replace the new console template

--- src/ConsoleArchivist/Program.cs
+++ src/ConsoleArchivist/Program.cs
@@ -1,6 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.Configuration;
 
+namespace ConsoleArchivist;
 
+public class Program
+{
+    public static IConfigurationRoot Configuration { get; set; }
 
-Console.WriteLine("Hello, World!");
+    public static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        Configuration = builder.Build();
+
+        // Aqui você pode continuar com a lógica do seu aplicativo,
+        // como instanciar o DbContext, etc.
+
+        Console.WriteLine("Hello, World!");
+    }
+
+}
